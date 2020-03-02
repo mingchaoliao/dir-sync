3f0ef92538bc0eaa03a523f575497708626f350d
Mingchao Liao <mingchaoliao95@gmail.com>
2020-03-01 19:28:55 -0500

    use readme file for pypi description

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import os
+
 from setuptools import setup
+
+if os.environ.get('CONVERT_README'):
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+else:
+    long_description = ''
 
 setup(
     name='dir-sync',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     description='Sync files in the local folder with files in the remote location, e.g. Google Drive.',
+    long_description=long_description,
     author='Mingchao Liao',
     author_email='mingchaoliao95@gmail.com',
     url='https://github.com/mingchaoliao/dir-sync',
